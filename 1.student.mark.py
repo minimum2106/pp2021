1cfc1bc13c7ad7c5015bec687097cc9ce6904c6c
unknown <tranhongquan2106@gmail.com>
2021-03-30 09:48:53 +0700

    code and result indentation

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -3,43 +3,50 @@ courses = []
 marks = []
 
 def stdNum():
-    num = int(input(" The number of students in the class: "))
+    num = int(input("- The number of students in the class: "))
     return num
 
 def stdInfo():
-    stdId = input("This student id: ")
-    stdName = input("This student name is: ")
-    stdDoB = input("This student DoB: ")
+    stdId = input("   + This student id: ")
+    stdName = input("   + This student name is: ")
+    stdDoB = input("   + This student DoB: ")
     return {"id": stdId, "name": stdName, "Dob": stdDoB}
 
 def courseNum():
-    num = int(input("The number of courses is: "))
+    num = int(input("- The number of courses is: "))
     return num
 
 def courseInfo():
-    courseId = input("This course id: ")
-    courseName = input("This course name is: ")
+    courseId = input("   + This course id: ")
+    courseName = input("   + This course name is: ")
     return {"id": courseId, "name": courseName}
 
 def insertCourseMark(course) :
     marks.append({course: {}})
     for i in range(len(students)):
-        mark = input("Mark of " + students[i]["name"] + " is: ")
+        mark = input("   + Mark of " + students[i]["name"] + " is: ")
         marks[len(marks) - 1][course].update({students[i]["name"]: validatedMark(mark)})
 
 def showCourses():
-    print("Courses \n" + str(courses))
+    print("Courses \n")
+    for course in courses:
+        print(str(course) + "\n")
 
 def showStudents():
-    print("Student \n"+ str(students))
+    print("Student \n")
+    for std in students:
+        print(str(std) + "\n")
 
 def showSpecificCourseMarks(course):
-    print("The mark of this course is: ")
+    print("- The mark of this course is: ")
     for i in range(len(marks)):
         markKeys = marks[i].keys()
         if course in markKeys:
-            print(marks[i])
+            # print(marks[i])
+            for name, mark in marks[i][course].items():
+                print("   + " + name + " : " + str(mark) + "\n")
 
+    
 def validatedMark(mark):
     try:
         check = float(mark)
@@ -55,35 +62,34 @@ def validateCourse(input):
             return True
     return False
 
-def run():
-    num = stdNum()
-    for i in range(num):
-        print("Student " + str(i + 1) + " info: ")
-        std = stdInfo()
-        students.append(std)
-    num = courseNum()
-    for i in range(num):
-        print("Course " + str(i + 1) + " info: ")
-        course = courseInfo()
-        courses.append(course)
-    enter = input("Do you want to enter the marks for any registered coures ? yes / no")
-    while(enter == "yes"):
-        inputCourse = input("The course you want to enter the marks is: ")
-        if(validateCourse(inputCourse)):
-            insertCourseMark(inputCourse)
-            enter = input("Do you want to enter the mark of other course ? yes / no")
-        else:
-            print("This course does not exist")
-            print("Do you want to re enter the name ?")
-            enter = input()
-    show = input("Do you want to see the student list and course list ? yes / no")
-    if (show == "yes"):
-        showCourses()
-        showStudents()
-        specificCourse = input("Enter the course you want to see the mark \n ")
-        if (validateCourse(specificCourse)):
-            showSpecificCourseMarks(specificCourse)
-        else:
-            print("You enter a wrong course !!!!")
-run() 
+num = stdNum()
+for i in range(num):
+    print("Student " + str(i + 1) + " info: ")
+    std = stdInfo()
+    students.append(std)
+num = courseNum()
+for i in range(num):
+    print("Course " + str(i + 1) + " info: ")
+    course = courseInfo()
+    courses.append(course)
+enter = input("Do you want to enter the marks for any registered coures ? yes / no ")
+while(enter == "yes"):
+    inputCourse = input("- The course you want to enter the marks is: ")
+    if(validateCourse(inputCourse)):
+        insertCourseMark(inputCourse)
+        enter = input("Do you want to enter the mark of other course ? yes / no ")
+    else:
+        print("This course does not exist")
+        print("Do you want to re enter the name ?")
+        enter = input()
+show = input("Do you want to see the student list and course list ? yes / no")
+if (show == "yes"):
+    showCourses()
+    showStudents()
+    specificCourse = input("Enter the course you want to see the mark \n ")
+    if (validateCourse(specificCourse)):
+        showSpecificCourseMarks(specificCourse)
+    else:
+        print("You enter a wrong course !!!!")
+
 
